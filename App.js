9ebedf109adb064853cc49b9d238339221d43b81
moahmed amine mezghich <git@mezghich.com>
2022-10-13 09:23:21 +0100

    systeme de navigation en stack

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// premier composant : Le systeme de navigation en Stack
+const Stack = createNativeStackNavigator();
+// deuxième composant : screen Home
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane', age:30 })
+      }
+    />
+  );
+};
+
+// troisième composant : screen profil
+const ProfileScreen = ({ navigation, route }) => {
+  return (
+    <View>
+      
+      <Text>This is {route.params.name}'s profile</Text>
+      
+      <Text>{route.params.name} age :  {route.params.age}</Text>
+      
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+      )};
+
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: 'My Profile' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
