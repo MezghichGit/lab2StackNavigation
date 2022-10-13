c4a44e558a28c2d81f7e30e9be3b918811766945
moahmed amine mezghich <git@mezghich.com>
2022-10-13 10:42:34 +0100

    tab de navigation avec icon

--- App.js
+++ App.js
@@ -2,18 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// Partie 1 :  Modèle de navigation en Stack
+/*
 // premier composant : Le systeme de navigation en Stack
 const Stack = createNativeStackNavigator();
 // deuxième composant : screen Home
 const HomeScreen = ({ navigation }) => {
   return (
+    <View>
     <Button
       title="Go to Jane's profile"
       onPress={() =>
         navigation.navigate('Profile', { name: 'Jane', age:30 })
       }
     />
+    <Button style={styles.btnMarge}
+      title="Logout"
+      onPress={() =>
+        navigation.navigate('Authentification')
+      }
+    />
+    </View>
   );
 };
 
@@ -46,7 +58,7 @@ const ProfileScreen = ({ navigation, route }) => {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Authentification' screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName='Authentification' screenOptions={{headerShown: true}}>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
         <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: 'My Profile' }} />
         <Stack.Screen name="Authentification" component={AuthentificationScreen} options={{ title: 'Connexion' }} />
@@ -63,4 +75,63 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  btnMarge: {
+    margin:10 ,
+  },
 });
+*/
+
+// Partie 2 :  Modèle de navigation en Tab
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();  //Création du Tab Navigator
+
+const MyTabs = () =>{
+  return (
+    <Tab.Navigator         
+    
+    screenOptions={({ route }) => ({
+      tabBarIcon: () => {
+        let iconName;
+        if (route.name == "Home") { iconName = "home-outline"; }
+        else if (route.name == "Settings") { iconName = "settings-outline"; }
+    
+
+        return (
+          <Ionicons
+            name={iconName}
+            color={'red'}
+            size={18}
+          />
+        );
+      },
+    })}
+>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
