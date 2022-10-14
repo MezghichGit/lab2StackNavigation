e2288d1bdd9ee2f900d0905c154bd2aa0be9aa4b
moahmed amine mezghich <git@mezghich.com>
2022-10-14 15:49:59 +0100

    geolocalisation avec MapView-Merker-Cercle

--- AppLocation.js
+++ AppLocation.js
@@ -1,32 +1,12 @@
 import * as React from 'react';
 import MapView, { Marker, Circle } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import * as Location from 'expo-location';
+
 export default function App() {
   const [pin, setPin] = React.useState({
     latitude: 37.296165925404495,
     longitude: 9.86878749496766
   })
-
-  React.useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      console.log(location);
-      setPin({ 
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude
-      })
-    })();
-  }, []);
-
-
   return (
     <View style={styles.container}>
       <MapView style={styles.map}
@@ -36,22 +16,14 @@ export default function App() {
           latitudeDelta: 0.005,
           longitudeDelta: 0.005,
         }}
-        showsUserLocation={true}
-        onUserLocationChange={(e) => { 
-          console.log("Position changed", e.nativeEvent.coordinate)
-          setPin({ 
-            latitude: e.nativeEvent.coordinate.latitude,
-            longitude: e.nativeEvent.coordinate.longitude
-          })
-       }}
       >
         <Marker
-          coordinate={pin}
+          coordinate={{ latitude: 37.296165925404495, longitude: 9.86878749496766 }}
           title="Centre ville"
           description="Bizerte North Africa"
           pinColor="gold"
           draggable
-          onDragStart={(e) => { console.log("Drag Start", e.nativeEvent.coordinate) }}
+          onDragStart={(e) => { console.log("Drag Start", e.nativeEvent) }}
           onDragEnd={(e) => { console.log("Drag End", e.nativeEvent.coordinate) 
           setPin({ 
             latitude: e.nativeEvent.coordinate.latitude,
